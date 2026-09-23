3ca6028d1f2b431e4186a29284d0b0aa4fe4c586
Jarad Inniss <jjinniss02@gmail.com>
2026-09-23 18:35:55 -0400

    First Commit

---
+++ lib/main.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'game.dart';
+
+void main() {
+  // runApp is a flutter SDK method that takes a widget argument and makes it the root of the widget tree
+  runApp(const MainApp());
+}
+
+// MainApp is the root widget in this case - the widget passed to runApp()
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Tile('A', HitType.none)
+        )
+      ),
+    );
+  }
+}
+
+class Tile extends StatelessWidget {
+  const Tile(this.letter, this.hitType, {super.key});
+
+  final String letter; // A String representing the guessed letter of the tile
+  final HitType hitType; // A HitType enum value represent the guess result and used to determine the color of the tile. For example, HitType.hit results in a green tile.
+
+  // The build() method must be defined for every widget and always returns another widget
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      /// Container is a convenience widget that wraps several core styling widgets,
+      /// such as Padding, ColoredBox, SizedBox, and DecoratedBox.
+      width: 60,
+      height: 60,
+      decoration: BoxDecoration(
+        /// BoxDecoration is an object that knows how to add any number of decorations to a widget,
+        /// from background color to borders to box shadows and more.
+        border: Border.all(color: Colors.grey.shade300),
+        borderRadius: BorderRadius.circular(8),
+        color: switch (hitType) {
+          HitType.hit => Colors.green,
+          HitType.partial => Colors.yellow,
+          HitType.miss => Colors.orange,
+          _ => Colors.grey
+        },
+      ),
+      /// the widget's child or children properties pass a widget or a list of widgets, respectively
+      child: Center(
+        child: Text(
+          letter.toUpperCase(),
+          style: Theme.of(context).textTheme.titleLarge,
+        ),
+      )
+    );
+  }
+}
